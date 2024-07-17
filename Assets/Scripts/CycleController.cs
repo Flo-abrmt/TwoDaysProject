9076204b0f3e2d56c90ac3a490670a04234543a9
DaDrogo <dark.drogo123@gmail.com>
2024-07-17 20:50:30 +0200

    added cycle time and controller functions

--- Assets/Scripts/CycleController.cs
+++ Assets/Scripts/CycleController.cs
@@ -1,24 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CycleController : MonoBehaviour
 {
     //end cylce open citycontroller and start building
 
-    [SerializeField] float time;
+    [SerializeField] int time = 0;
+    [SerializeField] int minTime = 0;
     [SerializeField] Sprite morningSprite;
-    [SerializeField] Sprite highnoonSprite;
+    [SerializeField] Sprite middaySprite;
     [SerializeField] Sprite eveningSprite;
+    [SerializeField] TMP_Text m_TextComponent;
+    [SerializeField] Image i_CycleView;
+
+    public bool repeatCycle = true;
 
     public CityController cityController;
     public void handleCycle() 
     {
-        endOfCycle();
+        minTime = minTime + 5;
+        if (minTime >= 60)
+        {
+            minTime = 0;
+            time = time + 1;
+        }
+
+
+        // Change the text on the text component.
+        m_TextComponent.text = time.ToString()+ ":"+minTime.ToString();
+        //endOfCycle();
+
+        if (time >= 24)
+        {
+            endOfCycle();
+        }
+
+        //startmorning
+        if(time > 6)
+        {
+            changeImage(morningSprite);
+        }
+        //startmidday
+        if(time >= 12)
+        {
+            changeImage(middaySprite);
+        }
+        //startevening
+        if (time >= 18)
+        {
+            changeImage(eveningSprite);
+        }
+        Debug.Log(minTime.ToString());
+        if (repeatCycle)
+        {
+            repeatCycle = false;
+            InvokeRepeating("handleCycle", 1f, 1f);
+        }
     }
     void startOfCycle()
     {
-
+        InvokeRepeating("handleCycle", 1.0f, 1.0f);
     }
 
     void middleOfCycle()
@@ -28,8 +73,12 @@ public class CycleController : MonoBehaviour
 
     void endOfCycle()
     {
+        CancelInvoke();
         cityController.whenCycle();
     }
 
-
+    void changeImage(Sprite image)
+    {
+        i_CycleView.sprite = image;
+    }
 }
---
+++ Assets/Scripts/Helper/Constants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    //one hour are 12 seconds
+    [SerializeField]public int HOUR = 12;
+    //one secound in rlt are 5 min in game
+    [SerializeField]public int SECOUND = 5;
+}
